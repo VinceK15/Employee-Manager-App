043813d000d67d5b38c5399557d0d8d44a638f6b
Vincent Kuan <vince_k372@yahoo.com>
2022-06-07 18:35:16 -0400

    adjusted logo and button position for mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,15 +13,15 @@ export default function Navbar() {
  return (
    <div>
      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
+       <NavLink className="navbar-brand ms-5 w-75" to="/">
          <img style={{"width" : 100 + '%'}} src={logo1}></img>
        </NavLink>
        
        <div className="navbar-collapse row justify-content-center alert alert-primary" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto w-50 justify-content-center">
-           <li className="nav-item" >
-             <NavLink className="nav-link" to="/create">
-               <button className="">Create Record</button>
+         <ul className="ms-5 navbar-nav w-50 justify-content-center">
+           <li className="nav-item justify-content-center" >
+             <NavLink className="nav-link justify-content-center" to="/create">
+               <button className="justify-content-center">Create Record</button>
              </NavLink>
            </li>
          </ul>
